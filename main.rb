cb4b9019bbfbfd0d9cfa52c4b2aa54a7d8c7130f
Karl Heitmann <khf1988@gmail.com>
2015-10-24 23:44:39 -0300

    funciona de forma basica

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
 require 'net/http'
-#curl -d "datum[bruto]=qqqqqqqqqq" localhost:3000/data
-uri = URI('http://')
+#curl -d "datum[bruto]=qqqqqqqqqq" 10.0.0.2:3000/data
+uri = URI('http://10.0.0.2:3000/data')
+res = Net::HTTP.post_form(uri, 'datum[bruto]' => 'raspi')
+puts res.body
