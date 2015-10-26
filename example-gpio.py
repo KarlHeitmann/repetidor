ddc918f3bd10b4133c2671ad6053c4b518c3ecdf
Karl Heitmann <khf1988@gmail.com>
2015-10-26 17:00:23 -0300

    agregado ejemplo blink, main.rb intentando utilizar libreria wiringpi, pero falla debido a que el wrapper para ruby no esta actyualizado para raspi2

--- example-gpio.py
+++ example-gpio.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
+PIN_A_USAR=11
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.IN)
+GPIO.setup(PIN_A_USAR, GPIO.IN)
 
 while True:
-  input_value = GPIO.input(12)
+  input_value = GPIO.input(PIN_A_USAR)
   if input_value == False:
     print("The button has been pressed.")
     while input_value == False:
-      input_value = GPIO.input(12)
+      input_value = GPIO.input(PIN_A_USAR)
 
