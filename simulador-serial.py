ad5b6306785ee6f5d1a08fe624d225b205eff403
Karl Heitmann <khf1988@gmail.com>
2016-01-03 16:56:20 -0300

    simulador grafica los datos de system

--- simulador-serial.py
+++ simulador-serial.py
@@ -28,17 +28,17 @@ import subprocess, time
 
 PROGRAMA = ['/usr/bin/gnuplot']
 NOMBRE_DESTINO = "respaldo"
+LIMITE = 100
 class Simulador:
     def __init__(self):
         self.gnuplot=subprocess.Popen(PROGRAMA,stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         self.data_parser={}
         self.f = open(NOMBRE_DESTINO, 'r')
+        self.n_data = 0
     def run(self):
         cuenta_linea=0
         for line in self.f:
             if ((cuenta_linea % 4)==1):
-                #print repr(line)
-                print line
                 self.data_parser["SYSTEM"] = line[0:3].encode('hex')
                 self.data_parser["START"] = line[4:7].encode('hex')
                 self.data_parser["CONECT"] = line[8:11].encode('hex')
@@ -50,8 +50,24 @@ class Simulador:
                 self.data_parser["BAT"] = line[46:50].encode('hex')
                 self.data_parser["ANG"] = line[51:55].encode('hex')
                 self.data_parser["HTH"] = line[56:60].encode('hex')
+                print line
                 print self.data_parser
+                if self.n_data == 100:
+                    self.plot()
+                self.n_data += 1
             cuenta_linea += 1
+    def plot(self):
+        frase = "plot '-' using 1:2\n"
+        print >>self.gnuplot.stdin, frase
+        for i in range(self.n_data):
+            frase = str(i) + " " + self.data_parser["SYSTEM"]
+            print >>self.gnuplot.stdin, frase
+        frase = "e\n"
+        print >>self.gnuplot.stdin, frase
+        time.sleep(5)
+
+        self.data_parser["SYSTEM"]
+
 def run():
     sim = Simulador()
     sim.run()
