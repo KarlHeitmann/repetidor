7ffe36aa6c7c7a96d39b9dff38f72bc93daafaca
Karl Heitmann <khf1988@gmail.com>
2016-01-03 14:47:56 -0300

    data parser decodifica los strings como hex

--- simulador-serial.py
+++ simulador-serial.py
@@ -27,8 +27,23 @@ CARACTERES ENVIADOS: 62
 NOMBRE_DESTINO = "respaldo"
 f = open(NOMBRE_DESTINO, 'r')
 cuenta_linea=0
+data_parser={}
 for line in f:
     if ((cuenta_linea % 4)==1):
-        print repr(line)
+        #print repr(line)
+        print line
+        data_parser["SYSTEM"] = line[0:3].encode('hex')
+        data_parser["START"] = line[4:7].encode('hex')
+        data_parser["CONECT"] = line[8:11].encode('hex')
+        data_parser["PERF"] = line[12:19].encode('hex')
+        data_parser["PWR"] = line[20:26].encode('hex')
+        data_parser["PERV"] = line[27:34].encode('hex')
+        data_parser["PFR"] = line[35:40].encode('hex')
+        data_parser["RED"] = line[41:45].encode('hex')
+        data_parser["BAT"] = line[46:50].encode('hex')
+        data_parser["ANG"] = line[51:55].encode('hex')
+        data_parser["HTH"] = line[56:60].encode('hex')
+        
+        print data_parser
     cuenta_linea += 1
 
