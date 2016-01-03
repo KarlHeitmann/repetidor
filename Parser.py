cf2e287ef7bd1c7a3dc490874827ac88f24a16ec
Karl Heitmann <khf1988@gmail.com>
2016-01-03 18:49:32 -0300

    incluido archivo parser, plotea un historial de los ultimos 100 datos

---
+++ Parser.py
@@ -0,0 +1,63 @@
+'''
+Apuntes: en la linea 238 de datatrans.c empieza a imprimir los caracteres
+ manda unsigned int de 3 espacios (si es mas largo los corta), separados por
+ el caracter '*'. Salvo los indicados a la derecha del nombre de variable. Recordar
+ que:
+ %3u -> imprime 3 caracteres del unsigned int, si es mas corto, hace un pad con espacios
+ blancos
+ %7lu -> 7 caracteres de long unsigned int
+
+Se trata de las variables siguientes (11 datos)
+
+SISTEM
+START
+CONECT
+SUMA = PERF5+PERF4...+PERF1 %7lu
+SUMA = PWR5 + ... + PWR1 %6lu
+SUMA = PERV5 + ... + PERV1 %7lu
+SUMA = PFR5 + ... + PFR1 %5lu
+SUMA = RED5 + ... + RED1 %4lu
+SUMA = BAT5 + ... + BAT1 %4lu
+SUMA = ANG5 + ... + ANG1 %4lu
+SUMA = HTH5 + ... + HTH1 %4lu
+
+
+CARACTERES ENVIADOS: 62
+'''
+
+class Parser:
+    def __init__(self, modo):
+        self.modo = modo
+        self.data = {"SYSTEM":[], "START":[], "CONECT":[], "PERF":[],
+        "PWR":[], "PERV":[], "PFR":[], "RED":[], "BAT":[], "ANG":[],
+        "HTH":[]}
+    def append(self, raw):
+        if (self.modo == "SIMULAR"):
+            self.data["SYSTEM"].append(raw[0*2:2*3].decode('hex'))
+            self.data["START"].append(raw[4*2:2*7].decode('hex'))
+            self.data["CONECT"].append(raw[8*2:2*11].decode('hex'))
+            self.data["PERF"].append(raw[12*2:2*19].decode('hex'))
+            self.data["PWR"].append(raw[20*2:2*26].decode('hex'))
+            self.data["PERV"].append(raw[27*2:2*34].decode('hex'))
+            self.data["PFR"].append(raw[35*2:2*40].decode('hex'))
+            self.data["RED"].append(raw[41*2:2*45].decode('hex'))
+            self.data["BAT"].append(raw[46*2:2*50].decode('hex'))
+            self.data["ANG"].append(raw[51*2:2*55].decode('hex'))
+            self.data["HTH"].append(raw[56*2:2*60].decode('hex'))
+        elif (self.modo == "RECIBIR"):
+            self.data["SYSTEM"] = raw[0:3]
+            self.data["START"] = raw[4:7]
+            self.data["CONECT"] = raw[8:11]
+            self.data["PERF"] = raw[12:19]
+            self.data["PWR"] = raw[20:26]
+            self.data["PERV"] = raw[27:34]
+            self.data["PFR"] = raw[35:40]
+            self.data["RED"] = raw[41:45]
+            self.data["BAT"] = raw[46:50]
+            self.data["ANG"] = raw[51:55]
+            self.data["HTH"] = raw[56:60]
+    def __getitem__(self, key):
+        return self.data[key][-1]
+    def get_all(self, key):
+        return self.data[key]
+
--- simulador-serial.py
+++ simulador-serial.py
@@ -1,30 +1,5 @@
-'''
-Apuntes: en la linea 238 de datatrans.c empieza a imprimir los caracteres
- manda unsigned int de 3 espacios (si es mas largo los corta), separados por
- el caracter '*'. Salvo los indicados a la derecha del nombre de variable. Recordar
- que:
- %3u -> imprime 3 caracteres del unsigned int, si es mas corto, hace un pad con espacios
- blancos
- %7lu -> 7 caracteres de long unsigned int
-
-Se trata de las variables siguientes (11 datos)
-
-SISTEM
-START
-CONECT
-SUMA = PERF5+PERF4...+PERF1 %7lu
-SUMA = PWR5 + ... + PWR1 %6lu
-SUMA = PERV5 + ... + PERV1 %7lu
-SUMA = PFR5 + ... + PFR1 %5lu
-SUMA = RED5 + ... + RED1 %4lu
-SUMA = BAT5 + ... + BAT1 %4lu
-SUMA = ANG5 + ... + ANG1 %4lu
-SUMA = HTH5 + ... + HTH1 %4lu
-
-
-CARACTERES ENVIADOS: 62
-'''
 import subprocess, time
+import Parser
 
 PROGRAMA = ['/usr/bin/gnuplot']
 NOMBRE_DESTINO = "respaldo"
@@ -32,24 +7,14 @@ LIMITE = 100
 class Simulador:
     def __init__(self):
         self.gnuplot=subprocess.Popen(PROGRAMA,stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        self.data_parser={}
+        self.data_parser=Parser.Parser("SIMULAR")
         self.f = open(NOMBRE_DESTINO, 'r')
         self.n_data = 0
     def run(self):
         cuenta_linea=0
         for line in self.f:
             if ((cuenta_linea % 4)==1):
-                self.data_parser["SYSTEM"] = line[0*2:2*3].decode('hex')
-                self.data_parser["START"] = line[4*2:2*7].decode('hex')
-                self.data_parser["CONECT"] = line[8*2:2*11].decode('hex')
-                self.data_parser["PERF"] = line[12*2:2*19].decode('hex')
-                self.data_parser["PWR"] = line[20*2:2*26].decode('hex')
-                self.data_parser["PERV"] = line[27*2:2*34].decode('hex')
-                self.data_parser["PFR"] = line[35*2:2*40].decode('hex')
-                self.data_parser["RED"] = line[41*2:2*45].decode('hex')
-                self.data_parser["BAT"] = line[46*2:2*50].decode('hex')
-                self.data_parser["ANG"] = line[51*2:2*55].decode('hex')
-                self.data_parser["HTH"] = line[56*2:2*60].decode('hex')
+                self.data_parser.append(line)
                 print type(self.data_parser["BAT"])
                 print self.data_parser["BAT"]
                 print ord(self.data_parser["BAT"][-2])
@@ -60,8 +25,9 @@ class Simulador:
     def plot(self):
         frase = "plot '-' using 1:2\n"
         print >>self.gnuplot.stdin, frase
+        data = self.data_parser.get_all("BAT")
         for i in range(self.n_data):
-            frase = str(i) + " " + self.data_parser["SYSTEM"]
+            frase = str(i) + " " + str(ord(data[i][-2]))
             print >>self.gnuplot.stdin, frase
         frase = "e\n"
         print >>self.gnuplot.stdin, frase
