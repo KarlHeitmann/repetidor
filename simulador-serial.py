7c253e1a9c413943addabaad02dfdd9c1da771b0
Karl Heitmann <khf1988@gmail.com>
2016-01-03 14:03:34 -0300

    lee el archivo de respaldo y saca lo que es necesario

---
+++ simulador-serial.py
@@ -0,0 +1,34 @@
+'''
+Apuntes: en la linea 238 de datatrans.c empieza a imprimir los caracteres
+ manda unsigned int de 3 espacios (si es mas largo los corta), separados por
+ el caracter '*'. Salvo los indicados a la derecha del nombre de variable. Recordar
+ que:
+ %3u -> imprime 3 caracteres del unsigned int, si es mas corto, hace un pad con espacios
+ blancos
+ %7lu -> 7 caracteres de long unsigned int
+
+Se trata de las variables siguientes (11 datos)
+
+SISTEM
+START
+CONECT
+SUMA = PERF5+PERF4...+PERF1 %7lu
+SUMA = PWR5 + ... + PWR1 %6lu
+SUMA = PERV5 + ... + PERV1 %7lu
+SUMA = PFR5 + ... + PFR1 %5lu
+SUMA = RED5 + ... + RED1 %4lu
+SUMA = BAT5 + ... + BAT1 %4lu
+SUMA = ANG5 + ... + ANG1 %4lu
+SUMA = HTH5 + ... + HTH1 %4lu
+
+
+CARACTERES ENVIADOS: 62
+'''
+NOMBRE_DESTINO = "respaldo"
+f = open(NOMBRE_DESTINO, 'r')
+cuenta_linea=0
+for line in f:
+    if ((cuenta_linea % 4)==1):
+        print repr(line)
+    cuenta_linea += 1
+
