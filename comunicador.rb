0e32da2876898e1583c10f7aef2059aa617cdf11
Karl Heitmann <khf1988@gmail.com>
2016-02-04 20:48:36 -0300

    envia el JSON

--- comunicador.rb
+++ comunicador.rb
@@ -1,17 +1,11 @@
 require 'net/http'
 require 'json'
-#require 'wiringpi2'
 require 'awesome_print'
 
-#DESCARGAR ESTO:
-#https://github.com/WiringPi/WiringPi-Ruby
-
-#data = JSON.parse(ARGV[0])
 ap ARGV[0]
-#data = JSON.parse('{"hello": "goodbye"}')
 data = JSON.parse(ARGV[0])
 ap data
 
 uri = URI('http://localhost:3000/data')
-res = Net::HTTP.post_form(uri, 'datum[bruto]' => ARGV[0])
-ap res.body
+res = Net::HTTP.post_form(uri, 'datum[bruto]' => data)
+#ap res.body
