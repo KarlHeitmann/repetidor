9430008ce52b4fdeec4dafa1444980e663fca835
Karl Heitmann <khf1988@gmail.com>
2016-02-07 12:24:10 -0300

    incorporada URL heroku

--- comunicador.rb
+++ comunicador.rb
@@ -6,6 +6,7 @@ ap ARGV[0]
 data = JSON.parse(ARGV[0])
 ap data
 
-uri = URI('http://localhost:3000/data')
-res = Net::HTTP.post_form(uri, 'datum[bruto]' => data)
+#uri = URI('http://localhost:3000/data')
+uri = URI('https://secure-eyrie-71751.herokuapp.com/data')
+res = Net::HTTP.post_form(uri, 'datum[bruto]' => data, 'datum[id]' => "1")
 #ap res.body
